82e5c52cf65355ef2ebd2fd29b710d99eef725ff
danilo pereira da silva <danilodpsnilo@outlook.com>
2024-06-11 21:23:51 -0300

    Set

---
+++ Aula244Set/src/application/Program1.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+      //Set<String> set = new TreeSet<>();
+	 //Set<String> set = new LinkedHashSet<>;
+		Set<String> set = new HashSet<>();
+		
+		set.add("TV");
+		set.add("Notebook");
+		set.add("Tablet");
+		
+		System.out.println(set.contains("Notebook"));
+		for (String p : set) {
+			System.out.println(p);
+		}
+	}
+}
